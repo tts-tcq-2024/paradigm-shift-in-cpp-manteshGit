f19042a7bec4b2efe28ed85ebc171493b536cd75
manteshGit <manteshappasaheb.eksambe@bosch.com>
2024-08-12 11:51:54 +0530

    Update statusUtils.h

--- statusUtils.h
+++ statusUtils.h
@@ -6,8 +6,14 @@
 #include <tuple>
 #include <map>
 
+// Define supported languages
+enum class Language {
+    ENGLISH,
+    GERMAN
+};
+
 std::map<StatusCode, std::string> createStatusDescriptionMap();
-std::string statusCodeToString(StatusCode status);
+std::string statusCodeToString(StatusCode status, Language lang = Language::ENGLISH);
 void printStatus(float soc, float temp, float chargeRate, StatusCode socStatus, StatusCode tempStatus, StatusCode chargeRateStatus);
 
 
