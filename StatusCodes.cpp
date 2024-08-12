3f3231949c400520e1e8c7ed984b54b33efebdb6
manteshGit <manteshappasaheb.eksambe@bosch.com>
2024-08-12 16:55:57 +0530

    Update StatusCodes.cpp

--- StatusCodes.cpp
+++ StatusCodes.cpp
@@ -1,12 +1,5 @@
 #include "StatusCodes.h"
 
-StatusCode Status::checkSOCBounds(float soc) const {
-    if (soc < SOC_MIN || soc > SOC_MAX) {
-        return SOC_OUT_OF_RANGE;
-    }
-    return NORMAL;
-}
-
 StatusCode Status::checkSOCWarnings(float soc) const {
     if (soc < SOC_MIN + SOC_WARNING_MIN_TOLERANCE) {
         return LOW_SOC_WARNING;
@@ -16,17 +9,17 @@ StatusCode Status::checkSOCWarnings(float soc) const {
     return NORMAL;
 }
 
-StatusCode Status::getSOCStatus(float soc) const {
-    StatusCode boundsStatus = checkSOCBounds(soc);
-    if (boundsStatus != NORMAL) return boundsStatus;
-    return checkSOCWarnings(soc);
-}
-
-StatusCode Status::checkTemperatureBounds(float temp) const {
-    if (temp > TEMP_MAX) {
-        return TEMP_OUT_OF_RANGE;
+StatusCode Status::checkSOCBounds(float soc) const {
+    if (soc < SOC_MIN || soc > SOC_MAX) {
+        return SOC_OUT_OF_RANGE;
     }
     return NORMAL;
+}
+
+StatusCode Status::getSOCStatus(float soc) const {
+    StatusCode warningStatus = checkSOCWarnings(soc);
+    if (warningStatus != NORMAL) return warningStatus;
+    return checkSOCBounds(soc);
 }
 
 StatusCode Status::checkTemperatureWarnings(float temp) const {
@@ -36,17 +29,17 @@ StatusCode Status::checkTemperatureWarnings(float temp) const {
     return NORMAL;
 }
 
-StatusCode Status::getTemperatureStatus(float temp) const {
-    StatusCode boundsStatus = checkTemperatureBounds(temp);
-    if (boundsStatus != NORMAL) return boundsStatus;
-    return checkTemperatureWarnings(temp);
-}
-
-StatusCode Status::checkChargeRateBounds(float chargeRate) const {
-    if (chargeRate > CHARGE_RATE_MAX) {
-        return CHARGE_RATE_OUT_OF_RANGE;
+StatusCode Status::checkTemperatureBounds(float temp) const {
+    if (temp > TEMP_MAX) {
+        return TEMP_OUT_OF_RANGE;
     }
     return NORMAL;
+}
+
+StatusCode Status::getTemperatureStatus(float temp) const {
+    StatusCode warningStatus = checkTemperatureWarnings(temp);
+    if (warningStatus != NORMAL) return warningStatus;
+    return checkTemperatureBounds(temp);
 }
 
 StatusCode Status::checkChargeRateWarnings(float chargeRate) const {
@@ -56,8 +49,15 @@ StatusCode Status::checkChargeRateWarnings(float chargeRate) const {
     return NORMAL;
 }
 
+StatusCode Status::checkChargeRateBounds(float chargeRate) const {
+    if (chargeRate > CHARGE_RATE_MAX) {
+        return CHARGE_RATE_OUT_OF_RANGE;
+    }
+    return NORMAL;
+}
+
 StatusCode Status::getChargeRateStatus(float chargeRate) const {
-    StatusCode boundsStatus = checkChargeRateBounds(chargeRate);
-    if (boundsStatus != NORMAL) return boundsStatus;
-    return checkChargeRateWarnings(chargeRate);
+    StatusCode warningStatus = checkChargeRateWarnings(chargeRate);
+    if (warningStatus != NORMAL) return warningStatus;
+    return checkChargeRateBounds(chargeRate);
 }
