853c4de7cdd0698da396e9fb6b1c76793693ee7e
manteshGit <manteshappasaheb.eksambe@bosch.com>
2024-08-13 18:40:55 +0530

    Update StatusCodes.h

--- StatusCodes.h
+++ StatusCodes.h
@@ -10,10 +10,25 @@ enum StatusCode {
     HIGH_SOC_WARNING = 3,
     HIGH_SOC_BREACH = 4,
     SOC_OUT_OF_RANGE = 5,
-    TEMP_OUT_OF_RANGE = 6,
-    TEMP_WARNING = 7,
+    TEMP_WARNING = 6,
+    TEMP_OUT_OF_RANGE = 7,
     CHARGE_RATE_WARNING = 8,
     CHARGE_RATE_OUT_OF_RANGE = 9
+};
+
+class StatusCodes {
+protected:
+    bool warningsEnabled = true; // Default to enabled
+
+public:
+    // Methods to enable/disable warnings
+    void setWarningsEnabled(bool enabled) {
+        warningsEnabled = enabled;
+    }
+
+    bool areWarningsEnabled() const {
+        return warningsEnabled;
+    }
 };
 
 #endif // STATUSCODES_H
