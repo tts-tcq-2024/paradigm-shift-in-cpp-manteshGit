53ced478755c8cf1eded33257157249c455f8a27
manteshGit <manteshappasaheb.eksambe@bosch.com>
2024-08-13 17:01:40 +0530

    Update StatusCodes.h

--- StatusCodes.h
+++ StatusCodes.h
@@ -16,30 +16,4 @@ enum StatusCode {
     CHARGE_RATE_OUT_OF_RANGE = 9
 };
 
-class Status {
-public:
-    static constexpr float SOC_MIN = 20.0f;
-    static constexpr float SOC_MAX = 80.0f;
-    static constexpr float SOC_WARNING_MAX_TOLERANCE = 0.05f * SOC_MAX;
-    static constexpr float SOC_WARNING_MIN_TOLERANCE = 0.05f * SOC_MAX;
-
-    static constexpr float TEMP_MAX = 45.0f;
-    static constexpr float TEMP_WARNING_TOLERANCE = 0.05f * TEMP_MAX;
-
-    static constexpr float CHARGE_RATE_MAX = 0.8f;
-    static constexpr float CHARGE_RATE_WARNING_TOLERANCE = 0.05f * CHARGE_RATE_MAX;
-
-    StatusCode checkSOCBounds(float soc) const;
-    StatusCode checkSOCWarnings(float soc) const;
-    StatusCode getSOCStatus(float soc) const;
-
-    StatusCode checkTemperatureBounds(float temp) const;
-    StatusCode checkTemperatureWarnings(float temp) const;
-    StatusCode getTemperatureStatus(float temp) const;
-
-    StatusCode checkChargeRateBounds(float chargeRate) const;
-    StatusCode checkChargeRateWarnings(float chargeRate) const;
-    StatusCode getChargeRateStatus(float chargeRate) const;
-};
-
 #endif // STATUSCODES_H
