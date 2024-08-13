e807bedf70d72d23075ec7bd9313314e577316b5
manteshGit <manteshappasaheb.eksambe@bosch.com>
2024-08-13 18:42:13 +0530

    Update ChargeRateStatus.h

--- ChargeRateStatus.h
+++ ChargeRateStatus.h
@@ -3,11 +3,14 @@
 
 #include "StatusCodes.h"  // Include for StatusCode enum
 
-class ChargeRateStatus {
+class ChargeRateStatus : public StatusCodes {
 private:
     static constexpr float CHARGE_RATE_MAX = 0.8f;
     static constexpr float CHARGE_RATE_WARNING_TOLERANCE = 0.05f * CHARGE_RATE_MAX;
 public:
+
+    ChargeRateStatus(bool enableWarnings = true);
+    
     StatusCode checkChargeRateWarnings(float chargeRate) const;
     StatusCode checkChargeRateBounds(float chargeRate) const;
     StatusCode getChargeRateStatus(float chargeRate) const;
