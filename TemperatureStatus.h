05b8ac232c1dc6ad118f46e1fc5eb722286873a4
manteshGit <manteshappasaheb.eksambe@bosch.com>
2024-08-13 18:41:56 +0530

    Update TemperatureStatus.h

--- TemperatureStatus.h
+++ TemperatureStatus.h
@@ -3,11 +3,13 @@
 
 #include "StatusCodes.h"  // Include for StatusCode enum
 
-class TemperatureStatus {
+class TemperatureStatus : public StatusCodes {
 private:
     static constexpr float TEMP_MAX = 45.0f;
     static constexpr float TEMP_WARNING_TOLERANCE = 0.05f * TEMP_MAX;
 public:
+    TemperatureStatus(bool enableWarnings = true);
+    
     StatusCode checkTemperatureWarnings(float temp) const;
     StatusCode checkTemperatureBounds(float temp) const;
     StatusCode getTemperatureStatus(float temp) const;
